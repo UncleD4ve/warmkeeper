d0f255082a4461c350da8e1da1acc674392b9906
exSHADe <pawelszymczyk18@gmail.com>
2021-06-07 00:16:42 +0200

    g

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -35,7 +35,7 @@ class UserService implements CRUD<User | string> {
     if (!(await bcrypt.compare(resource.password, userContext.password)) && resource.password !== "" 
     && resource.password !== null && resource.password !== undefined )
     {
-      if(!(this.validator.test(resource.password))) 
+      if(this.validator.test(resource.password) == false) 
       throw new HttpException(400, "Password need to contain at least 8 characters with 1 uppercase,lowercase,digit and special characters")
       hash = await bcrypt.hash(resource.password, 10);
     }
